9b11a2bd3a21e2201eec5f675b13a7c42190b88f
Yury Michurin <yury@automattic.com>
2016-11-28 16:14:23 +0200

    Wrote comments for AutoDirection

--- client/components/auto-direction/index.jsx
+++ client/components/auto-direction/index.jsx
@@ -10,6 +10,11 @@ import userModule from 'lib/user';
 
 const user = userModule();
 
+/***
+ * Gets text content from react element in case that's a leaf element
+ * @param react element
+ * @returns {string|null} returns a text content of the react element or null if it's not a leaf element
+ */
 const getContent = ( { props: reactComponentProps } ) => {
 	// The child is a text node
 	if ( typeof reactComponentProps.children === 'string' ) {
@@ -37,6 +42,14 @@ const getContent = ( { props: reactComponentProps } ) => {
 // Copied from: https://github.com/twitter/RTLtextarea/blob/master/src/RTLText.module.js#L46
 const rtlChar = /[\u0590-\u083F]|[\u08A0-\u08FF]|[\uFB1D-\uFDFF]|[\uFE70-\uFEFF]/mg;
 const rtlThreshold = 0.3;
+
+/***
+ * Gets the main directionality in a text
+ * It returns what kind of characters we had the most, RTL or LTR according to some ratio
+ *
+ * @param {string} text the text to be examined
+ * @returns {string} either 'rtl' or 'ltr'
+ */
 const getTextMainDirection = ( text ) => {
 	let rtlCount = 0;
 	for ( let i = 0; i < text.length; i++ ) {
@@ -46,6 +59,15 @@ const getTextMainDirection = ( text ) => {
 	return ( rtlCount / text.length > rtlThreshold ) ? 'rtl' : 'ltr';
 };
 
+/***
+ * Sets a react component child directionality according to it's text content
+ * That function intended to be used recursively with React.Children.map
+ * It will set directionality only to the leaf components - because it does so according
+ * to text content and only leaf components have those.
+ *
+ * @param {React.Element} child
+ * @returns {React.Element} transformed child
+ */
 const setChildDirection = ( child ) => {
 	const childContent = getContent( child );
 
@@ -71,8 +93,13 @@ const setChildDirection = ( child ) => {
 	} );
 };
 
-const AutoDirection = ( props ) => {
-	const directionedChildren = React.Children.map( props.children, setChildDirection );
+/***
+ * Auto direction component that will set direction to child components according to their text content
+ * @param {Object.children} props must contain some children
+ * @returns {ReactElement}
+ */
+const AutoDirection = ( { children } ) => {
+	const directionedChildren = React.Children.map( children, setChildDirection );
 
 	return <div>{ directionedChildren }</div>;
 };
