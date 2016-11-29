e9e98938a080443951353270729f8bb6a144d8e3
Copons <copons@users.noreply.github.com>
2016-11-29 18:23:34 +0000

    Ship the new editor insert menu to production along with a Guided Tour

--- client/layout/guided-tours/config.js
+++ client/layout/guided-tours/config.js
@@ -3,7 +3,9 @@
  */
 import { combineTours } from 'layout/guided-tours/config-elements';
 import { MainTour } from 'layout/guided-tours/main-tour';
+import { EditorInsertMenuTour } from 'layout/guided-tours/editor-insert-menu-tour';
 
 export default combineTours( {
 	main: MainTour,
+	'editor-insert-menu': EditorInsertMenuTour,
 } );
